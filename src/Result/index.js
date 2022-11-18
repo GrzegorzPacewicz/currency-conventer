b67bb5372d7b909e9c20ca01fcf8af1327665c8c
Grzegorz Pacewicz <g.pacewicz@gmail.com>
2022-11-18 15:21:18 +0100

    Result text

--- src/Result/index.js
+++ src/Result/index.js
@@ -3,10 +3,7 @@ import "./style.css"
 const Result = ({ result }) => (
 
     <p className="result">
-        Za tę kwotę otrzymasz: 
-        <strong>
-            {result}
-        </strong>
+        Za tę kwotę otrzymasz: <strong>{result}</strong>
     </p>
 );
 
