d3463bd90293d19e0ab86575ff217d4d607bae1b
Grzegorz Pacewicz <g.pacewicz@gmail.com>
2022-12-07 09:42:34 +0100

    useCurrentDate hook fixed

--- src/Clock/useCurrentDate.js
+++ src/Clock/useCurrentDate.js
@@ -3,15 +3,6 @@ import { useState, useEffect } from "react";
 export const useCurrentDate = () => {
   const [date, setDate] = useState(new Date());
 
-  const formatDate = date.toLocaleString("pl", {
-    day: "numeric",
-    month: "numeric",
-    year: "numeric",
-    hour: "2-digit",
-    minute: "2-digit",
-    second: "2-digit",
-  });
-
   useEffect(() => {
     const intervalId = setInterval(() => {
       setDate(new Date());
@@ -22,5 +13,5 @@ export const useCurrentDate = () => {
     };
   }, []);
 
-  return formatDate;
+  return date;
 };
