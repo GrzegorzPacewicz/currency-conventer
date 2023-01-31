be47337b471dbda28c3a859a9acf035e8ca1016d
Grzegorz Pacewicz <86358105+GrzegorzPacewicz@users.noreply.github.com>
2023-01-31 23:14:32 +0100

    ThemeSwitch function made simpler

--- src/ThemeSwitch/index.js
+++ src/ThemeSwitch/index.js
@@ -4,15 +4,12 @@ import SunIcon from "./icons/SunIcon";
 import MoonIcon from "./icons/MoonIcon";
 
 const ThemeSwitch = ({ toggleTheme, isDarkTheme }) => {
-  const [checked, setChecked] = useState();
-  const toggler = () => (checked ? setChecked(false) : setChecked(true));
 
   const [isToggled, setIsToggled] = useState(isDarkTheme);
 
   const onToggle = () => {
     setIsToggled(!isToggled);
     toggleTheme();
-    toggler();
   };
 
   return (
