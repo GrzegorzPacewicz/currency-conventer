d4d8165016e8eb12f6313ae6248fb90b26005ef3
Grzegorz Pacewicz <g.pacewicz@gmail.com>
2023-02-01 16:24:53 +0100

    UselocalStorage for theme

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
-import Form from "./Form";
-import Header from "./Header";
-import Clock from "./Clock";
-import Footer from "./Footer";
+import Form from "./features/Form";
+import Header from "./features/Header";
+import Clock from "./features/Clock";
+import Footer from "./features/Footer";
 import ThemeSwitch from "./ThemeSwitch";
-import { Container } from "./Container/styled";
+import { Container } from "./features/Container/styled";
 import { ThemeProvider } from "styled-components";
 import { GlobalStyle } from "./GlobalStyle";
 import { lightTheme, darkTheme } from "./theme";
 import { useState } from "react";
+import { useEffect } from "react";
+
+const getLocalStorageTheme = () => {
+  const localStorageTheme = localStorage.getItem("theme");
+  return localStorageTheme ? JSON.parse(localStorageTheme) : [];
+};
 
 function App() {
-  const [theme, setTheme] = useState("light");
+  const [theme, setTheme] = useState(getLocalStorageTheme);
   const isDarkTheme = theme === "dark";
+
+  useEffect(() => {
+    localStorage.setItem("theme", JSON.stringify(theme));
+  }, [theme]);
 
   const toggleTheme = () => {
     setTheme(isDarkTheme ? "light" : "dark");
